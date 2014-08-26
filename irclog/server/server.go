fe2fbf95c1ffbff0c74821055a9c222142f60156
Boqun Feng <boqun.feng@gmail.com>
2014-08-26 22:17:04 +0800

    irclog: enable dated view
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- irclog/server/server.go
+++ irclog/server/server.go
@@ -13,6 +13,7 @@ import (
 )
 
 var location *time.Location
+var oneDay time.Duration
 
 const (
 	PAGE_SIZE = 50
@@ -29,6 +30,8 @@ func min(a, b int64) int64 {
 
 func main() {
 	location, _ = time.LoadLocation("Asia/Shanghai")
+
+	oneDay, _ = time.ParseDuration("24h")
 
 	mux := routes.New()
 	mux.Get("/", index)
@@ -54,12 +57,43 @@ func validChannel(cname string) bool {
 func msgCount(cname string) int64 {
 	client, err := redis.Dial("tcp", "127.0.0.1:6379")
 	defer client.Close()
-	num, err := client.Cmd("ZCOUNT", Key("channel", cname, "queue"), 0, (1 << 63) - 1).Int64()
+	// ZCARD is O(1) operation
+	num, err := client.Cmd("ZCARD", Key("channel", cname, "queue")).Int64()
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return -1
 	}
 	return num
+}
+
+func msgDate(cname string, index int64) time.Time {
+	client, err := redis.Dial("tcp", "127.0.0.1:6379")
+	defer client.Close()
+
+	msgs, err := client.Cmd("ZRANGE", Key("channel", cname, "queue"), index, index).List()
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return time.Now().In(location)
+	}
+
+	nano, err := client.Cmd("HGET", msgs[0], "time").Int64()
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return time.Now().In(location)
+	}
+
+	return time.Unix(0, nano).In(location)
+
+}
+
+func msgStartDate(cname string) time.Time {
+	return msgDate(cname, 0)
+}
+
+func msgEndDate(cname string) time.Time {
+	return msgDate(cname, -1)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -78,6 +112,16 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Channel: <a href='/channel/%s'>%s</a><br/>", channel, channel)
 		count := msgCount(channel)
 
+		fmt.Fprintf(w, "By Date:<br/>\n")
+
+		for date := msgStartDate(channel); date.Before(msgEndDate(channel)); date = date.Add(oneDay) {
+			fmt.Fprintf(w, "<a href='/channel/%s/date/%s'>%s</a> ", channel,
+				date.Truncate(oneDay).Format("2006/01/02"),
+				date.Truncate(oneDay).Format("2006/01/02"))
+		}
+		fmt.Fprintf(w, "<br/>")
+
+		fmt.Fprintf(w, "By Page:<br/>\n")
 		for i := int64(0); i < count; i += PAGE_SIZE {
 			fmt.Fprintf(w, "<a href='/channel/%s/page/%v'>%v~%v</a> ", channel,
 				i / PAGE_SIZE, i, min(i+PAGE_SIZE-1, count-1))
@@ -93,7 +137,7 @@ func allChannelMsg(w http.ResponseWriter, r *http.Request) {
 	cname := params.Get(":cname")
 	if (validChannel(cname)) {
 		fmt.Fprintf(w, "<!doctype html><html><body>")
-		channel(w, cname, 0, -1)
+		channel(w, cname, 0, -1, false)
 		fmt.Fprintf(w, "</html></body>")
 	} else {
 		fmt.Fprintf(w, `This channel is not logged now,
@@ -132,7 +176,7 @@ func pagedChannelMsg(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<a href='/'>Home</a>")
 		fmt.Fprintf(w, "<br/>\n")
 
-		channel(w, cname, pageNo * PAGE_SIZE, (pageNo + 1) * PAGE_SIZE - 1)
+		channel(w, cname, pageNo * PAGE_SIZE, (pageNo + 1) * PAGE_SIZE - 1, false)
 		fmt.Fprintf(w, "</html></body>")
 	} else {
 		fmt.Fprintf(w, "This channel is not logged now," +
@@ -142,23 +186,55 @@ func pagedChannelMsg(w http.ResponseWriter, r *http.Request) {
 }
 
 func datedChannelMsg(w http.ResponseWriter, r *http.Request) {
-	//params := r.URL.Query()
-	//cname := params.Get(":cname")
-	//year := params.Get(":year")
-	//month := params.Get(":month")
-	//day := params.Get(":day")
+	params := r.URL.Query()
+	cname := params.Get(":cname")
+	year := params.Get(":year")
+	month := params.Get(":month")
+	day := params.Get(":day")
 
-	//date, err := time.ParseInLocation("2006/01/02",
-	//	fmt.Sprintf("%s/%s/%s", year, month, day),
-	//	location)
-	//if err != nil {
-//		fmt.Fprintf(w, "wrong date format")
-//	}
+	date, err := time.ParseInLocation("2006/01/02",
+		fmt.Sprintf("%s/%s/%s", year, month, day),
+		location)
+	if err != nil {
+		fmt.Fprintf(w, "wrong date format")
+	}
 
-	//TODO: I'm too hungry to write code
+	start := msgStartDate(cname)
+	end := msgEndDate(cname)
+
+	fmt.Fprintf(w, "<!doctype html><html><body>")
+
+	fmt.Fprintf(w, "<a href='/channel/%s/date/%s'>First</a>",
+		cname, start.Truncate(oneDay).Format("2006/01/02"))
+	fmt.Fprintf(w, " ")
+
+	if date.AddDate(0, 0, -1).After(start) {
+		fmt.Fprintf(w, "<a href='/channel/%s/date/%s'>Prev</a>",
+			cname, date.AddDate(0, 0, -1).Format("2006/01/02"))
+		fmt.Fprintf(w, " ")
+	}
+
+	if date.AddDate(0, 0, 1).Before(end) {
+		fmt.Fprintf(w, "<a href='/channel/%s/date/%s'>Next</a>",
+			cname, date.AddDate(0, 0, -1).Format("2006/01/02"))
+		fmt.Fprintf(w, " ")
+	}
+
+	fmt.Fprintf(w, "<a href='/channel/%s/date/%s'>Last</a>",
+		cname, end.Truncate(oneDay).Format("2006/01/02"))
+	fmt.Fprintf(w, " ")
+
+	fmt.Fprintf(w, "<a href='/channel/%s'>Full</a>", cname)
+	fmt.Fprintf(w, " ")
+	fmt.Fprintf(w, "<a href='/'>Home</a>")
+	fmt.Fprintf(w, "<br/>\n")
+
+	channel(w, cname, date.UnixNano(), date.AddDate(0, 0, 1).UnixNano() - 1, true)
+	fmt.Fprintf(w, "</html></body>")
+
 
 }
-func channel(w http.ResponseWriter, cname string, start, end int64) {
+func channel(w http.ResponseWriter, cname string, start, end int64, byScore bool) {
 	client, err := redis.Dial("tcp", "127.0.0.1:6379")
 	defer client.Close()
 
@@ -168,7 +244,13 @@ func channel(w http.ResponseWriter, cname string, start, end int64) {
 
 	tmpl, _ := template.New("msg").Parse("{{.left}} {{.middle}} {{.right}}<br/>")
 	line := map[string]string{"left": "", "middle": "", "right": "",}
-	msgs, _ := client.Cmd("ZRANGE", Key("channel", cname, "queue"), start, end).List()
+
+	var msgs []string
+	if byScore {
+		msgs, _ = client.Cmd("ZRANGEBYSCORE", Key("channel", cname, "queue"), start, end).List()
+	} else {
+		msgs, _ = client.Cmd("ZRANGE", Key("channel", cname, "queue"), start, end).List()
+	}
 	for _, msg := range msgs {
 		item, _ := client.Cmd("HGETALL", msg).Hash()
 		msgType, _ := strconv.Atoi(item["type"])
